1e4d15a0add9fe054f5c53f0819911cd0ab6007f
Allen Tran <allentran08@gmail.com>
2021-09-30 16:41:33 -0700

    push before erasing

--- app/Pages/index.py
+++ app/Pages/index.py
@@ -2,9 +2,9 @@ from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__, template_folder='../templates')
 
-@app.route("/<name>")
-def home(name):
-    return render_template("index.html", content = name, r= 2)
+@app.route("/home")
+def home():
+    return render_template("home.html")
 
 if __name__ == "__main__":
     app.run(debug = True)
