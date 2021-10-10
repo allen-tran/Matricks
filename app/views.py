bdc29b9f9faecf7a4d8aa66dbd28641a1c0b2fba
Allen Tran <allentran08@gmail.com>
2021-10-09 22:53:18 -0700

    switching tabs works

--- app/views.py
+++ app/views.py
@@ -6,11 +6,27 @@ views = Blueprint('views', __name__)
 
 @views.route('/')
 def home():
-    return render_template("base.html")
+    return render_template("addition.html")
 
 @views.route('/addition')
 def addition():
     return render_template("addition.html")
+
+@views.route('/multiplication')
+def multiplication():
+    return render_template("multiplication.html")
+
+@views.route('/subtraction')
+def subtraction():
+    return render_template("subtraction.html")
+
+@views.route('/determinant')
+def determinant():
+    return render_template("determinant.html")
+
+@views.route('/inverse')
+def inverse():
+    return render_template("inverse.html")
 
 @views.route("/add",methods = ['POST'])
 def add():
@@ -35,9 +51,9 @@ def add():
         for j in range(len(b[i])):
             x = int(input("Enter element: "))
             b[i][j] = x
-    return render_template("base.html", sum = np.add(a, b))
+    return render_template("addtion.html", sum = np.add(a, b))
 
-@views.route('/multiplication', methods="POST")
+@views.route('/mult', methods=["POST"])
 def mult():
     m = int(request.form['dim'])
     n = int(request.form['dim2'])
@@ -61,10 +77,4 @@ def mult():
             x = int(input("Enter element: "))
             b[i][j] = x
     return render_template("base.html", sum = np.add(a, b))
-
-def multiplication():
-    return render_template("multiplication.html")
     
-@views.route('/inverse')
-def inverse():
-    return render_template("inverse.html")
